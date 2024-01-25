89cbe217d11dc4e79137b0e603730c78e93aa960
icehongssii12345 <icehongssii@gmail.com>
2024-01-25 18:06:35 +0900

    IDWFW-60 땅 staticbody로 변경

--- player.gd
+++ player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+const SPEED = 10
 const JUMP_VELOCITY = -400.0
 @onready var anim = get_node("AnimationPlayer")
 # Get the gravity from the project settings to be synced with RigidBody nodes.
